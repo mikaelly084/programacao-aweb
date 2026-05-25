7a34b64a16600c08ae465572e7677b680d2f4381
mikaelly084 <mikaellyalmeida084@gmail.com>
2026-05-25 19:05:03 +0000

    Feat: Implementa o perfil do aluno e pagina sobre

---
+++ Uninove.Web/Controllers/Controllers/AlunoController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Uninove.Web.Models;
+
+namespace Uninove.Web.Controllers;
+
+public class AlunoController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewBag.Nome = "Seu Nome Aqui";
+        ViewBag.Curso = "Análise e Desenvolvimento de Sistemas";
+        ViewBag.Semestre = "3º Semestre";
+
+        return View();
+    }
+
+    public IActionResult Detalhes(int id)
+    {
+         ViewBag.AlunoId = id;
+        
+        return View(index.cshtml);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
